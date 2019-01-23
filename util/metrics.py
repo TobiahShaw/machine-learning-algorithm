ec71d68402c922e29d2ad14e6a7494fa3739f5ae
TobiahShaw <a5414994@outlook.com>
2019-01-23 16:05:46 +0800

    sort of note; add func of assess linear regression

--- util/metrics.py
+++ util/metrics.py
@@ -4,3 +4,16 @@ def accuracy_score(y_true, y_predict):
     assert y_true.shape == y_predict.shape,\
         "the size of y_true must be equal to the size of y_predict"
     return (sum(y_true == y_predict) / len(y_true))
+
+def mean_squarred_error(y_true, y_predict):
+    assert len(y_true) == len(y_predict),\
+        "the size of y_true must be equal to the size of y_predict"
+    return np.sum((y_true - y_predict) ** 2) / len(y_true)
+
+def root_mean_squarred_error(y_true, y_oredict):
+    return np.sqrt(mean_squarred_error(y_true, y_oredict))
+
+def mean_absolute_error(y_true, y_predict):
+    assert len(y_true) == len(y_predict),\
+        "the size of y_true must be equal to the size of y_predict"
+    return np.sum(np.abs(y_true - y_predict)) / len(y_true)
